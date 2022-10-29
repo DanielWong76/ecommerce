6d45b4875ff19613036e6ca74c5e14a81daa572c
Daniel Wong <djw329@cornell.edu>
2022-10-29 19:06:38 -0400

    Started NavBar

---
+++ src/navigation/navigation.js
@@ -0,0 +1,24 @@
+import logo from './static/REAPER.jpg';
+import './index.css'
+
+export default function Navigation() {
+    return(
+        <nav className = "nav">
+            <a href="/" className = "site-logo"><img className="logo" src={logo} width={100} height={100} alt="logo"></img></a>
+            <ui>
+                <li>
+                    <a href="/Shop">Shop</a>
+                </li>
+                <li>
+                    <a href="/About">About</a>
+                </li>
+                <li>
+                    <a href="/Contact">Contact</a>
+                </li>
+                <li>
+                    <a href="/Login">Login</a>
+                </li>
+            </ui>
+        </nav>
+    );
+}
